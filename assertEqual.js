303a6766471f1c7393373fa80d1c61399939ba99
Maddy Tan <maddytan7@gmail.com>
2022-05-02 18:18:06 +0000

    created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("LighthouseLabs", "Bootcamp");
+assertEqual(1, 1);

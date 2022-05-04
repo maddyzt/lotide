8a437f5eabc5ed0d41d4e5de9cd1c731e9221638
Maddy Tan <maddytan7@gmail.com>
2022-05-04 20:15:12 +0000

    initial commit. created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  let listKeys = Object.keys(object);
+  let listValues = Object.values(object);
+  for (let i of listKeys) {
+    if (object[i] === value) {
+      return i;
+    }
+  }
+};
+
+// // test code
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// // console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

342d18c55ad08e760ec72f519aa529465fb5c112
Maddy Tan <maddytan7@gmail.com>
2022-05-09 01:45:18 +0000

    removed extra g character

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-g
+
 describe("#middle", () => {
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);

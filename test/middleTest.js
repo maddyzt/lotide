bd018d6dbb682aaf868ea25e38c40becfbfb14d7
Maddy Tan <maddytan7@gmail.com>
2022-05-09 01:24:29 +0000

    removed extra g character from middleTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-g
+
 // test cases
 console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
 console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));

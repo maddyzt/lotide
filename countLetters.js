ac69c7e47c9284e258172491adfd725e16ff23b4
Maddy Tan <maddytan7@gmail.com>
2022-05-09 17:08:07 +0000

    added CountLettersTest file

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const countLetters = (stringToCount) => {
   const newObject = {};
@@ -18,8 +12,7 @@ const countLetters = (stringToCount) => {
   return newObject;
 };
 
-// test code
+module.exports = countLetters;
 
-// console.log(countLetters("maddy tan!"))
 
 
---
+++ test/countLettersTest.js
@@ -0,0 +1,6 @@
+const countLetters = require('../countLetters');
+
+// test code
+console.log(countLetters("lighthouse in the house"));
+
+g

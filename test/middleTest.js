5598a04475eb00faee35a156db545ef09a9f1190
Maddy Tan <maddytan7@gmail.com>
2022-05-09 01:44:49 +0000

    added mocha/chai to middleTest.js file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,18 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+g
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-// test cases
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2]), []));
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    const result = middle([1, 2]);
+    assert.equal(result.length, 0);
+  })
+});

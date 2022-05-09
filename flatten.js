f8604d831e0889bdb2a3b5b6f92eeebc1000a565
Maddy Tan <maddytan7@gmail.com>
2022-05-09 17:36:16 +0000

    added flattenTest file

--- flatten.js
+++ flatten.js
@@ -12,7 +12,4 @@ const flatten = (array) => {
   return newArray;
 };
 
-// console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
-// console.log(flatten([1, [2, 3, 4], 5, [6], [7]]));
-// console.log(flatten([1, 2, [3, 4], 5, [6], 7, 8, 9, 10]));
-// console.log(flatten([[1], 2, [3], [4], 5, 6, 7, [8], 9, 10, 11, 12]));
+module.exports = flatten;
